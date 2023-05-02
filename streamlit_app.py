f3abab2a5935072aae01beae37c9d34b299078c1
Aaron Genovia <38897766+agenovia@users.noreply.github.com>
2023-05-02 15:55:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ list(map(st.text, items))
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list.set_index("Fruit", drop=True, inplace=True)
 
 # put  pick list
 st.multiselect("Pick some fruits:", my_fruit_list.index.tolist())
