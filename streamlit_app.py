0094fcfc990016fe994c29d5b4fb9cb77d3490bb
Aaron Genovia <38897766+agenovia@users.noreply.github.com>
2023-05-02 15:37:02 -0700

    Update streamlit_app.py
    add menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Favorites')
+
+items = [
+          '🥣 Omega 3 & Blueberry Oatmeal',
+          '🥗 Kale, Spinach & Rocket Smoothie',
+          '🐔 Hard-Boiled Free-Range Egg',
+          '🥑🍞 Avocado Toast'
+]
+map(lambda x: streamlit.text(x), items)
